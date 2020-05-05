64faf8e530710e745a7e28ebbea83c065f92d5dd
Sarala Retter <saralaretter@gmail.com>
2020-05-05 10:14:32 -0400

    Add project files.

---
+++ React/ClientApp/src/MyFirstComponent.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import MySecondComponent from './MySecondComponent';
+
+class MyFirstComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>A Story</h1>
+                <MySecondComponent />
+            </div>
+        );
+    }
+}
+
+export default MyFirstComponent;
---
+++ React/ClientApp/src/MySecondComponent.js
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+
+class MySecondComponent extends React.Component {
+    render() {
+        return <p>Once upon a time...</p>;
+    }
+}
+
+export default MySecondComponent;
